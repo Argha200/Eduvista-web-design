d4df49bcde9db3629172f2543abca511205fb52a
Argha200 <107509647+Argha200@users.noreply.github.com>
2024-03-26 01:27:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function home_section_call(){
+    document.getElementById('header_section_id').scrollIntoView({
+        behavior: 'smooth' 
+    });
+}
+
+function about_section_call(){
+    document.getElementById('about_section_id').scrollIntoView({
+        behavior: 'smooth' 
+    });
+}
+
+function courses_section_call(){
+    document.getElementById('courses_section_id').scrollIntoView({
+        behavior: 'smooth' 
+    });
+}
+
+function instructors_section_call(){
+    document.getElementById('instructors_section_id').scrollIntoView({
+        behavior: 'smooth' 
+    });
+}
